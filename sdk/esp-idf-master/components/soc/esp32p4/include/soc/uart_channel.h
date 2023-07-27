5f7a143b35647be3a695f402584d8f2fade9aedf
rx-ted <1552488649@qq.com>
2023-07-27 14:36:43 +0800

    update .gitignore

--- sdk/esp-idf-master/components/soc/esp32p4/include/soc/uart_channel.h
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// This file defines GPIO lookup macros for available UART IO_MUX pins on ESP32C6.
-
-#pragma once
--- sdk/esp-idf-master/docs/zh_CN/api-reference/peripherals/clk_tree.rst
+++
@@ -1,1 +0,0 @@
-.. include:: ../../../en/api-reference/peripherals/clk_tree.rst
